cb0a05bef082b05687b80da4f66895ca8c2944a7
Matt Jones <matt.r.jones1995@gmail.com>
2021-01-09 16:43:19 +0000

    Fix build error due to no fallback

--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -1,7 +1,16 @@
+import { useRouter } from "next/dist/client/router";
 import { getMovies } from "../api/movies";
 import { getMovie } from "../api/movies/[pid]";
 
 const Movie = ({ movie }) => {
+  const router = useRouter();
+
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1>{movie.title}</h1> <p>{movie.fullplot}</p>
